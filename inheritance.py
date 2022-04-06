d3dda93b075cafef4f28d7df6100adc3caca7911
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-04-06 12:58:30 +0800

    First Commit

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    def __init__(self, vehicle_type, vehicle_no, place_made, year_made) -> None:
+        self.vehicle_type = vehicle_type
+        self.vehicle_no = vehicle_no
+        self.place_made = place_made
+        self.year_made = year_made
+    
+    def __str__(self) -> str:
+        pass
+
+    
+
+class Car():
+    
+    def request(self):
+
+
+
+
