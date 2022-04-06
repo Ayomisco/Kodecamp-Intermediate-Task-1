1c20257082fd25f062aca6d44dda542094469738
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-04-06 14:19:57 +0800

    Editted the Car class

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,24 @@
 class Vehicle:
-    def __init__(self, vehicle_type, vehicle_no, place_made, year_made) -> None:
+    def __init__(self, name, vehicle_type, vehicle_no, place_made, year_made) -> None:
+        self.name = name
         self.vehicle_type = vehicle_type
         self.vehicle_no = vehicle_no
         self.place_made = place_made
         self.year_made = year_made
     
-    def __str__(self) -> str:
-        pass
+    def __str__(self):
+        return f"Name: {self.name}, Vehicle Type: {self.vehicle_type}, Place Made: {self.place_made}, Vehicle No.: {self.vehicle_no}, Year Made: {self.year_made}"
 
     
 
-class Car():
+class Car(Vehicle):
     
     def request(self):
+        pass
+
+toyota = Car("Toyota Camry", "Toyota", "Russia", "002332200", "2016")
+
+print(toyota)
 
 
 
